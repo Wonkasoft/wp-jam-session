442ffdba6b36fd332f9762b1ee3f6e275a085d46
MrLister <rlister@wonkasoft.com>
2017-07-20 14:22:05 -0700

    MrLister refactoring

--- admin/partials/wp-jam-session-settings-tab.php
+++ admin/partials/wp-jam-session-settings-tab.php
@@ -1,12 +1,10 @@
 <?php
 /**
  * Provide a markup area for the settings form for the plugin
- *
  * This file is used to markup the settings form aspects of the plugin.
  *
  * @link       https://wonkasoft.com
  * @since      1.0.0
- *
  * @package    Wp_Jam_Session
  * @subpackage Wp_Jam_Session/admin/partials
  */
@@ -15,7 +13,7 @@
 <div id="settings-tab" class="tab-pane fade in active">
   <div class="row title-row">
     <div class="col-xs-12 col-md-3">
-      <img class="img-responsive block-center jam-logo" src="<?php echo plugins_url('/wp-jam-session/admin/img/jam-session-logo.png'); ?>" />
+      <img class="img-responsive block-center jam-logo" src="<?php echo plugins_url( '/wp-jam-session/admin/img/jam-session-logo.png' ); ?>" />
     </div>
     <div class="col-xs-12 col-md-8">
       <h1>WP JAM SESSION</h1>
@@ -31,7 +29,7 @@
             <div class="form-group">
               <label for="url-para">Add URL Parameter: </label>
               <p>example: ?yourParameterHere= <span data-toggle="tooltip" data-placement="top" title="This would be where you would put your custom parameter." class="help-badge"><strong>?</strong></span></p>
-              <input type="text" name="url-para" class="form-control" id="url-para" value="<?php echo get_option('wp-jam-session-url-para') ?>" maxlength="15">
+              <input type="text" name="url-para" class="form-control" id="url-para" value="<?php echo get_option( 'wp-jam-session-url-para' ) ?>" maxlength="15">
             </div> <!-- end form-group -->
             </div> <!-- end of this items col -->
             <div class="col-xs-12 col-md-6">
@@ -47,14 +45,14 @@
               <p>example: Select form plugin <span data-toggle="tooltip" data-placement="top" title="Please select the type of form that you are using." class="help-badge"><strong>?</strong></span></p>
               <select type="select" name="type-form" class="form-control" id="type-form">
               <?php  
-                $selected_option = get_option('wp-jam-session-type-form');
-                switch ($selected_option) {
+                $selected_option = get_option( 'wp-jam-session-type-form' );
+                switch ( $selected_option ) {
                   case 'WooCommerce': ?>
                     <option value="" disabled>Select your option</option>
                     <option value="WooCommerce" selected>WooCommerce</option>
                     <option value="Contact Form 7">Contact Form 7</option>
                     <option value="Ninja Forms">Ninja Forms</option>
-                    <option value="Gravity Forms">Gravity Forms</option>
+                    <option value="Gravity Forms">Gravity Forms - pro version only</option>
                     <?php
                     break;
 
@@ -63,7 +61,7 @@
                     <option value="WooCommerce">WooCommerce</option>
                     <option value="Contact Form 7" selected>Contact Form 7</option>
                     <option value="Ninja Forms">Ninja Forms</option>
-                    <option value="Gravity Forms">Gravity Forms</option>
+                    <option value="Gravity Forms">Gravity Forms - pro version only</option>
                     <?php
                     break;
 
@@ -72,7 +70,7 @@
                     <option value="WooCommerce">WooCommerce</option>
                     <option value="Contact Form 7">Contact Form 7</option>
                     <option value="Ninja Forms" selected>Ninja Forms</option>
-                    <option value="Gravity Forms">Gravity Forms</option>
+                    <option value="Gravity Forms">Gravity Forms - pro version only</option>
                     <?php
                     break;
 
@@ -81,7 +79,7 @@
                     <option value="WooCommerce">WooCommerce</option>
                     <option value="Contact Form 7">Contact Form 7</option>
                     <option value="Ninja Forms">Ninja Forms</option>
-                    <option value="Gravity Forms" selected>Gravity Forms</option>
+                    <option value="Gravity Forms" selected>Gravity Forms - pro version only</option>
                     <?php
                     break;
                   
@@ -90,7 +88,7 @@
                   <option value="WooCommerce">WooCommerce</option>
                   <option value="Contact Form 7">Contact Form 7</option>
                   <option value="Ninja Forms">Ninja Forms</option>
-                  <option value="Gravity Forms">Gravity Forms</option>
+                  <option value="Gravity Forms">Gravity Forms - pro version only</option>
                   <?php
                     break;
                 }
@@ -120,7 +118,7 @@
                 'option_none_value' => get_site_url(),
                 'value_field' => 'guid',
                 );
-              wp_dropdown_pages($pages_dropdown); 
+              wp_dropdown_pages( $pages_dropdown ); 
               ?>
             </div> <!-- end form-group -->
             </div> <!-- end of this items col -->
@@ -137,7 +135,7 @@
               <p>You can quickly copy this by clicking the clipboard button. <span data-toggle="tooltip" data-placement="top" title="You can quickly copy this link by clicking the button to the right of the url." class="help-badge"><strong>?</strong></span></p>
               <div class="input-group">
               <div class="input-group-btn copy-btn-div">
-              <input name="created-url" id="created-url" class="form-control" value="" disabled><button id="copy-btn-id" class="btn btn-info" type="button"><i class="glyphicon glyphicon-copy copy-btn pull-right"></i></button>
+              <input name="created-url" id="created-url" class="form-control" value="" readonly="true" =""><button id="copy-btn-id" class="btn btn-info" type="button"><i class="glyphicon glyphicon-copy copy-btn pull-right"></i></button>
               </div> <!-- end button wrap -->
               </div> <!-- end input-group -->
             </div> <!-- end form-group -->
